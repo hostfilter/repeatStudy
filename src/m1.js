cbd0927de1f041a1d92ca51a3cc092465ca5ca30
gec-hostfilter <gec-625656210@qq.com>
2022-10-19 19:01:16 +0800

    1.将所有文件上传操作

---
+++ src/m1.js
@@ -0,0 +1,15 @@
+export let a=10
+
+export let obj={
+  name:'历程',
+  age:15,
+  sex:'女'
+}
+
+export let fn=function(){
+  console.log('init操作')
+}
+
+export let arr=[23,'sdfj',fn,{name:'刘庆',age:24}]
+
+export let test='我这个怎么出bug了'
